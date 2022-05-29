9bad052de9154a2ebd4f1a49ca805937e83e1080
yuka120 <yuka.cherry.0926@icloud.com>
2022-05-29 17:43:41 +0900

    完成

--- src/views/QuizApp.vue
+++ src/views/QuizApp.vue
@@ -1,25 +1,26 @@
 <template>
   <h1>Vue クイズ</h1>
   <div class="app">
-    <h2>Q. {{ quiz.text }}</h2>
+    <h2>Q. {{ format.text }}</h2>
     <img
       class="quiz-image"
-      src="@/assets/気になるリンゴ.jpg"
+      :src="require(`@/assets/${format.quizes[quizNumber].image}.jpg`)"
       alt="お土産クイズ"
     />
     <div class="container">
-      <button v-on:click="choiced(0)">
+      <button v-on:click="choiced(0)" v-if="buttonSeen">
         {{ format.quizes[quizNumber].choices[0].text }}
       </button>
-      <button v-on:click="choiced(1)">
+      <button v-on:click="choiced(1)" v-if="buttonSeen">
         {{ format.quizes[quizNumber].choices[1].text }}
       </button>
-      <button v-on:click="choiced(2)">
+      <button v-on:click="choiced(2)" v-if="buttonSeen">
         {{ format.quizes[quizNumber].choices[2].text }}
       </button>
     </div>
     <div>{{ feedback }}</div>
-    <button v-if="seen">次の問題へ</button>>
+    <button v-if="seen" v-on:click="changeQuiz">次の問題へ</button>
+    <div v-if="finish">これで問題は終わりだよ</div>
   </div>
 </template>
 
@@ -30,11 +31,13 @@ export default {
       feedback: "",
       quizNumber: 0,
       seen: false,
+      buttonSeen: true,
+      finish: false,
       format: {
         text: "このおみやげは何県の名物でしょう",
         quizes: [
           {
-            image: "気になるリンゴ.jpg",
+            image: "気になるリンゴ",
             choices: [
               {
                 text: "青森県",
@@ -55,7 +58,7 @@ export default {
             ],
           },
           {
-            image: "桔梗信玄餅.jpg",
+            image: "桔梗信玄餅",
             choices: [
               {
                 text: "福岡県",
@@ -74,17 +77,74 @@ export default {
               },
             ],
           },
+          {
+            image: "赤い靴",
+            choices: [
+              {
+                text: "神奈川県",
+                isCorrect: true,
+                feedback:
+                  "正解！横浜を舞台にした童謡「赤い靴」をモチーフにしたチョコレート菓子だよ。",
+              },
+              {
+                text: "広島県",
+                isCorrect: false,
+                feedback: "残念！広島県はもみじ饅頭がおいしいよね。",
+              },
+              {
+                text: "奈良県",
+                isCorrect: false,
+                feedback: "残念！奈良県は柿の葉寿司が人気だよ",
+              },
+            ],
+          },
+          {
+            image: "おやき",
+            choices: [
+              {
+                text: "東京",
+                isCorrect: false,
+                feedback: "残念！東京は東京バナナでしょ！",
+              },
+              {
+                text: "長野県",
+                isCorrect: true,
+                feedback: "正解！長野県のおやき、おいしそうだね！",
+              },
+              {
+                text: "熊本県",
+                isCorrect: false,
+                feedback: "残念！くまもんが怒っちゃうよ💦",
+              },
+            ],
+          },
         ],
       },
     }
   },
   methods: {
     choiced(i) {
-      this.seen = true
-      this.feedback = this.format.quizes[this.quizNumber].choices[i].feedback
+      if (this.format.quizes[this.quizNumber].choices[i].isCorrect) {
+        if (this.quizNumber <= 2) {
+          this.seen = true
+          this.buttonSeen = false
+          this.feedback =
+            this.format.quizes[this.quizNumber].choices[i].feedback
+        } else {
+          this.buttonSeen = false
+          this.feedback =
+            this.format.quizes[this.quizNumber].choices[i].feedback
+          this.finish = true
+        }
+      } else {
+        this.feedback = this.format.quizes[this.quizNumber].choices[i].feedback
+      }
     },
     changeQuiz() {
       this.quizNumber += 1
+      this.feedback = ""
+      this.seen = false
+      this.buttonSeen = true
     },
   },
 }
